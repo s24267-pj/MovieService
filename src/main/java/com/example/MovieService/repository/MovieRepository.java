d7944a73b5833ac535528070ede1f74928b7da69
damianligeza <s24267@pjwstk.edu.pl>
2023-05-27 08:38:13 +0200

    Zjazd 5 - PD - dodac zmiane isAvailable

--- src/main/java/com/example/MovieService/repository/MovieRepository.java
+++ src/main/java/com/example/MovieService/repository/MovieRepository.java
@@ -2,6 +2,7 @@ package com.example.MovieService.repository;
 
 import com.example.MovieService.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +11,10 @@ import java.util.Optional;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+@Query("SELECT movie FROM Movie movie WHERE movie.id = ?1")
     Optional<Movie> findById(Long aLong);
 
-
+@Query("select movie from Movie movie")
     List<Movie> findAll();
 
 
